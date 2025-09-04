65acd6fcba87e0ce8b553a8b129b07150ff9ef0f
bishwa <108havocx108@gmail.com>
2025-09-04 19:39:35 +0545

    Added the form for the task

--- src/app/components/task-form.tsx
+++ src/app/components/task-form.tsx
@@ -1,15 +1,25 @@
+"use client";
 import React from "react";
 import TaskList from "./task-list";
+import useTaskStore from "../stores/task-store";
+import DialogueBox from "./dialogue-box";
+import { AnimatePresence } from "motion/react";
 
 const TaskForm = () => {
+  const isFormOpen = useTaskStore((state) => state.isFormOpen);
+  const openForm = useTaskStore((state) => state.openForm);
+
   return (
-    <div className=" flex justify-center max-w-[60%] min-h-screen mx-auto bg-[#060625] p-8 border-x-1 border-red-50/20">
+    <div className=" flex justify-center sm:max-w-[60%] min-h-screen sm:mx-auto bg-[#060625] p-8 border-x-1 border-red-50/20">
       <div className="flex flex-col items-center mt-4  w-full">
-        <h1 className="text-2xl sm:text-4xl tracking-wide capitalize">
+        <h1 className="mt-2 font-bold text-3xl sm:text-4xl tracking-wide capitalize">
           task manager
         </h1>
-        <div className="flex justify-between items-center w-2/3 h-4 mt-4 sm:mt-8">
-          <button className="bg-gray-700 rounded-md px-3 py-1 cursor-pointer hover:bg-green-700 transition-all">
+        <div className="flex justify-between items-center w-xs sm:w-xl mt-12">
+          <button
+            className="bg-gray-700 rounded-md px-3 py-1 cursor-pointer hover:bg-green-700 transition-all"
+            onClick={openForm}
+          >
             Add Task
           </button>
           <select className="bg-gray-700 rounded-md px-2 py-1 shadow focus:outline-none  cursor-pointer">
@@ -19,9 +29,11 @@ const TaskForm = () => {
           </select>
         </div>
 
-        <div className="bg-gray-500 w-[90%] rounded-md mt-6 sm:mt-14 p-2 min-h-[10vh] ">
+        <div className="bg-gray-500 w-sm sm:w-2xl rounded-md mt-4 sm:mt-4 p-2 min-h-[10vh] ">
           <TaskList />
         </div>
+        {/* for exiting and enterign animations */}
+        <AnimatePresence>{isFormOpen && <DialogueBox />}</AnimatePresence>
       </div>
     </div>
   );
