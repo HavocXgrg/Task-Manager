45e47a7043c8f12066e53912c8f793d5d683e6ef
bishwa <108havocx108@gmail.com>
2025-09-05 13:37:23 +0545

    updated the code and integrated react toastify

--- src/app/components/dialogue-box.tsx
+++ src/app/components/dialogue-box.tsx
@@ -1,6 +1,8 @@
+"use client";
 import React, { useState } from "react";
 import useTaskStore from "../stores/task-store";
 import { motion } from "motion/react";
+import { toast } from "react-toastify";
 
 const DialogueBox = () => {
   const closeForm = useTaskStore((state) => state.closeForm);
@@ -9,11 +11,15 @@ const DialogueBox = () => {
   const [title, setTitle] = useState("");
   const [status, setStatus] = useState<"incomplete" | "complete">("incomplete");
 
-  const handleAdd = (e: React.FormEvent) => {
+  const handleAddTask = (e: React.FormEvent) => {
     e.preventDefault();
     const trimmedTitle = title.trim();
-    if (!trimmedTitle) return;
-    addTask(trimmedTitle, status === "complete"); //this === assignment helps to return boolean
+    if (!trimmedTitle) {
+      toast.error("Task title cannot be empty!");
+      return;
+    }
+    addTask(trimmedTitle, status === "complete");
+    toast.success(`Task added successfully!`);
     setTitle("");
     closeForm();
   };
@@ -28,11 +34,13 @@ const DialogueBox = () => {
       >
         <div className="flex flex-col gap-4 w-full">
           <h2 className="text-2xl font-bold text-white mb-2">Add New Task</h2>
-          <form>
+
+          <form onSubmit={handleAddTask}>
             <label className="flex flex-col text-xl text-gray-200">
               Title
               <input
                 type="text"
+                autoFocus
                 className="bg-slate-800 h-10 text-gray-300 text-base font-light p-2 mb-6 mt-2 rounded-md outline-none focus:ring-2 focus:ring-blue-400"
                 value={title}
                 onChange={(e) => setTitle(e.target.value)}
@@ -56,13 +64,15 @@ const DialogueBox = () => {
 
           <div className="mt-6 flex gap-4">
             <button
+              type="submit"
+              onClick={handleAddTask}
               className="bg-green-700 p-2 px-4 text-white rounded-md cursor-pointer hover:bg-green-600 transition-all"
-              onClick={handleAdd}
             >
               Add Task
             </button>
 
             <button
+              type="button"
               className="bg-gray-600 p-2 px-4 text-white rounded-md cursor-pointer hover:bg-gray-500 transition-all"
               onClick={closeForm}
             >
